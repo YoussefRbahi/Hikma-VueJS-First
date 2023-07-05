ff59618f0d3bf5f00d3d09c9d459ea685e9ac445
Youssef Rbahi <youssefrbahi@gmail.com>
2023-07-05 22:44:31 +0300

    router not working

--- Hikma/src/router/index.js
+++ Hikma/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import UniversityPage from '../components/uni'
+import Home from '../components/Home.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +8,12 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: Home
+    },
+    {
+      path: '/university/:id',
+      name: 'university',
+      component: UniversityPage
     }
   ]
 })
